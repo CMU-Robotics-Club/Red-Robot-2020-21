f0119142590e550ccc05de2842d5c3f2d97cd7a3
Ravi Dudhagra <rdcosmicwonders@gmail.com>
2020-10-28 14:23:47 -0400

    found a bug in the line sensor code
    -pinmodes set to OUTPUT instead of INPUT
    -changes made
    -behavior shouldn't change, for some reason the code worked on output as well, but technically that isn't correct

--- line_follower/line_sensor_functions.ino
+++ line_follower/line_sensor_functions.ino
@@ -2,8 +2,8 @@
 #define RIGHT_LINE_SENSOR 10
 
 void line_sensor_setup() {
-  pinMode(LEFT_LINE_SENSOR, OUTPUT);
-  pinMode(RIGHT_LINE_SENSOR, OUTPUT);
+  pinMode(LEFT_LINE_SENSOR, INPUT);
+  pinMode(RIGHT_LINE_SENSOR, INPUT);
 }
 
 int read_left_sensor() {
